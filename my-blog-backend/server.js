d5f92f26d4b069663e785e932cf26d9ec7cfe477
agstn71 <augustinehsp33@gmail.com>
2025-08-28 18:53:28 +0530

    create blog-website backend

---
+++ my-blog-backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectdb from "./database/db.js";
+import userRoute from "./routes/user.route.js"
+import blogRoute from "./routes/blog.route.js"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+dotenv.config()
+const app = express()
+app.use(express.json());
+app.use(cookieParser())
+app.use(express.urlencoded({extended:true}))
+app.use(cors({
+    origin:"https://blog-website-beige-nu.vercel.app/",
+    credentials:true
+}))
+
+const PORT = process.env.PORT || 3000
+
+app.use("/api/v1/user",userRoute)
+app.use("/api/v1/blog",blogRoute)
+
+app.listen(PORT,() => {
+    connectdb()
+    console.log(`server listen at port ${PORT}`);
+    
+})
