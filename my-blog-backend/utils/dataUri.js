ad008d1f81b7896e7ef3bb4f47a401ad5f91ef36
agstn71 <augustinehsp33@gmail.com>
2025-08-28 22:10:32 +0530

    fix issue

--- my-blog-backend/utils/dataUri.js
+++ my-blog-backend/utils/dataUri.js
@@ -1,11 +1,11 @@
-import DataUriParser from "dataUri/parser.js"
-import path from "path"
+import DataUriParser from "datauri/parser.js";
+import path from "path";
 
-const parser = new DataUriParser()
+const parser = new DataUriParser();
 
 const getDataUri = (file) => {
-     const extName = path.extname(file.originalname).toString();
-     return parser.format(extName,file.buffer).content
-}
+  const extName = path.extname(file.originalname).toString();
+  return parser.format(extName, file.buffer).content;
+};
 
-export default getDataUri
+export default getDataUri;
