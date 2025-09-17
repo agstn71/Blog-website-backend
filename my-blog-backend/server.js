e608c64ce24c000130450f5c03809736d004a4fe
agstn71 <augustinehsp33@gmail.com>
2025-09-17 22:43:32 +0530

    change origin

--- my-blog-backend/server.js
+++ my-blog-backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin:"*",
+    origin:"https://blog-agstn.vercel.app",
     credentials:true
 }))
 
